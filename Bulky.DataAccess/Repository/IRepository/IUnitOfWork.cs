8ee548bde8f3e2c9dcfa0e576d65ba79b7a8b35d
vedpatel7 <vedvirpara2004@gmail.com>
2023-10-26 00:56:16 +0530

    new commit

---
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Book.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookWeb.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
+
+        void Save();
+    }
+}
